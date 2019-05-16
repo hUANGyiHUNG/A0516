0851fede2b48e88de89c93ff3e17bea7dccefefb
CJCU <h24661109@mailst.cjcu.edu.tw>
2019-05-16 13:42:23 +0800

    加入Spinner的資料

--- app/src/main/java/com/example/cjcu/a123/MainActivity.java
+++ app/src/main/java/com/example/cjcu/a123/MainActivity.java
@@ -2,14 +2,18 @@ package com.example.cjcu.a123;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+
+import java.lang.reflect.Array;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
     private Spinner sp_fruit;
 
+    String[] fruit={"apple","banana","奇異果"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,5 +21,7 @@ public class MainActivity extends AppCompatActivity {
         button = findViewById(R.id.button);
         sp_fruit = findViewById(R.id.sp_fruit);
 
+        ArrayAdapter adapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,fruit);
+        sp_fruit.setAdapter(adapter);
     }
 }
